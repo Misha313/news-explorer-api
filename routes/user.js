4d98dcfdfa6cbf8d44484f2bf1911af3b004fddf
Misha Memus <fedarovichmikhail@gmail.com>
2020-10-04 11:59:32 +0300

    фикс роутера инфы о пользователе

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,6 @@ const { createUserCheck, loginCheck } = require('../middlewares/validation');
 
 userRouter.post('/signup', createUserCheck, createUser);
 userRouter.post('/signin', loginCheck, login);
-userRouter.get('users/me', auth, showMyInfo);
+userRouter.get('/users/me', auth, showMyInfo);
 
 module.exports = userRouter;
