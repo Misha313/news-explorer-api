1f58f59174705ee0b4960e129cb91136b541a1c5
Misha Memus <fedarovichmikhail@gmail.com>
2020-10-04 12:17:53 +0300

    fix

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const auth = require('../middlewares/auth');
 
 const { createUserCheck, loginCheck } = require('../middlewares/validation');
 
+userRouter.get('/users/me', auth, showMyInfo);
 userRouter.post('/signup', createUserCheck, createUser);
 userRouter.post('/signin', loginCheck, login);
-userRouter.get('/users/me', auth, showMyInfo);
 
 module.exports = userRouter;
