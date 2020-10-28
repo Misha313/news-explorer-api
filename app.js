0f0177f7a26af3d980cbb7cffdb47c3f8176305b
Misha Memus <fedarovichmikhail@gmail.com>
2020-10-28 22:23:45 +0300

    cors

--- app.js
+++ app.js
@@ -24,15 +24,16 @@ mongoose.connect(NODE_ENV === 'production' ? DATABASE_LINK : 'mongodb://localhos
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080/');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
 app.use(requestLogger);
 
-app.use(router, cors());
+app.use(router);
 
 app.use(errorLogger);
 
